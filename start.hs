e0074feb9be6338d1dddcbdab7b580e2090ad3a3
Fasand <mail@petrmanas.name>
2016-12-02 11:47:59 +0000

    Added terminal-size and implemented it

--- start.hs
+++ start.hs
@@ -4,10 +4,17 @@ import Control.Concurrent
 import Control.Monad
 import Data.Maybe
 import System.IO
+import System.Console.Terminal.Size
 
 main = do
     c <- newEmptyMVar
     hSetBuffering stdin NoBuffering
+
+    -- Get Terminal size
+    s <- size
+    let (w, h) = (\(Just x) -> (width x, height x)) s
+    --putStrLn (show w ++ " " ++ show h)
+
     forkIO $ do
       key <- getChar
       putMVar c key
