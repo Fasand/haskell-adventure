d3919d8310770fa7883d1fb3efdd35ac52bc2813
Fasand <mail@petrmanas.name>
2016-12-02 11:36:53 +0000

    Added keypress support

--- start.hs
+++ start.hs
@@ -1,4 +1,19 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello World"
+import Control.Concurrent
+import Control.Monad
+import Data.Maybe
+import System.IO
+
+main = do
+    c <- newEmptyMVar
+    hSetBuffering stdin NoBuffering
+    forkIO $ do
+      key <- getChar
+      putMVar c key
+      putStrLn $ "\nChar:" ++ [key]
+    wait c
+  where wait c = do
+          a <- tryTakeMVar c
+          if isJust a then main
+          else threadDelay 200000 >> wait c
